489122d81e1e0d5bf2c53da1e38d868b570e5bbd
GoGo-DevS <diegodinamarca.a@gmail.com>
2026-05-03 16:06:49 -0400

    Configuración SMTP Zoho para formulario de contacto

--- travesia/settings.py
+++ travesia/settings.py
@@ -125,9 +125,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtppro.zoho.com'
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_TIMEOUT = int(os.getenv('EMAIL_TIMEOUT', '10'))
 
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
